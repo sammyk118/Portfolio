d4795dd59663b6e2ec8c864609f24ddf4d05cf77
Sammy Kroner <sammyk118@gmail.com>
2021-04-05 12:12:34 -0700

    fixed json file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,13 @@
 import React from "react";
-import profile from "../../assets/Profile.jpg"
 
 function Home() {
-    console.log(profile)
     return (
         <div className=" small-up-2 off-canvas position-left reveal-for-large is-transition-push is-closed" id="my-info" data-off-canvas=""
             aria-hidden="true" data-e="sxdx0q-e">
             <div className="grid-y grid-padding-x" id="sideBar">
                 <br />
                 <div className="cell shrink">
-                    <img className="thumbnail" src={profile} alt="profile"/>
+                    <img className="thumbnail" src="https://raw.githubusercontent.com/sammyk118/Portfolio/main/public/assets/Profile.jpg" alt="profile"/>
                 </div>
                 <div className="cell auto">
                     <p>Sammy is an aspiring web developer with a degree in computer science, and experience in JavaScript, C++, React, express, HTML, SQL, and Server Side APIs.</p>
